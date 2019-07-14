7cf03072f0fcf12f5fddbcc30adff6644350c6de
ddotx <domemacintosh@gmail.com>
2019-07-14 10:43:42 +0700

    update comment

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,16 +1,19 @@
-import {item, Item} from './item.model'
+import {Item} from './item.model'
 import mongoose from 'mongoose'
 import {connect} from '../../utils/db'
 
 //TODO==> Controller perform CRUD on the models based on the routes + verbs.
+//*==> Controllers implement the logic --> DB models
+//*==> REST Approach require CRUD actions on resource
+
 const run = async () =>{
     await connect('mongodb://localhost:27017/api-test') //!any db string url
 
     //TODO==> C === model.create(), new model()
     const item = await Item.create({
         name: 'Clean up',
-        createdBy: mongoose.Types.ObjectId(), //!fake
-        list: mongoose.Types.ObjectId() //!fake
+        createdBy: mongoose.Types.ObjectId(), //!create fake
+        list: mongoose.Types.ObjectId() //!create fake
     })
 
     console.log(item)
@@ -28,9 +31,9 @@ const run = async () =>{
     console.log(updated)
 
     //TODO==> D === model.remove(), model.findByIdAndRemove(), model.findOneAndRemove()
-    const remove = await Item.findByIdAndRemove(item._id).exec()
+    const removed = await Item.findByIdAndRemove(item._id).exec()
     
-    console.log(remove)
+    console.log(removed)
 
 }
 
